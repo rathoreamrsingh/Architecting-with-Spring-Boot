a7c0820bfc28a3fe6babf646c37aeeec9064cd6b
Amar Singh <amarsingh@outlook.in>
2018-09-13 15:07:21 +0530

    amar|added gateway service

---
+++ gateway-service/src/main/java/com/demo/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.demo.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+}
