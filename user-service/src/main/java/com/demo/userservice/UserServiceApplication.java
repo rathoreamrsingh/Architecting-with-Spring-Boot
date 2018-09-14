35a6430d4c1330694793e4d6df9eb23e60e69166
Amar Singh <amarsingh@outlook.in>
2018-09-15 03:22:00 +0530

    Amar|added configuration server and hystrix

--- user-service/src/main/java/com/demo/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/demo/userservice/UserServiceApplication.java
@@ -2,7 +2,13 @@ package com.demo.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
+@EnableHystrix
 @SpringBootApplication
 public class UserServiceApplication {
 
--- user-service/src/main/java/com/demo/userservice/controller/UserController.java
+++ user-service/src/main/java/com/demo/userservice/controller/UserController.java
@@ -4,7 +4,9 @@ import com.demo.userservice.model.LoginDetail;
 import com.demo.userservice.model.User;
 import com.demo.userservice.model.UserWithOrder;
 import com.demo.userservice.service.UserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
@@ -17,6 +19,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("${message:Hello default}")
+    private String message;
 
     @GetMapping("/getAllUsers")
     public List<User> getAllUsers() {
@@ -45,10 +50,29 @@ public class UserController {
         return result;
     }
 
+    @GetMapping("/getUserWithOrdersWithHystix/{username}")
+    @HystrixCommand(groupKey = "getUserWithOrder", commandKey = "getUserWithOrder", fallbackMethod = "fallback")
+    public UserWithOrder getUserWithOrdersWithHystix(@PathVariable(value = "username") String username) {
+        UserWithOrder result;
+        result = userService.getUserWithUser(username);
+        return result;
+    }
+
+    public UserWithOrder fallback(@PathVariable(value = "username") String username) {
+        UserWithOrder result = new UserWithOrder();
+        User u = userService.getUserByUsername(username);
+        result.setUser(u);
+        return result;
+    }
     @PostMapping("/login")
     public Boolean login(@RequestBody LoginDetail loginDetail) {
         Boolean result;
         result = userService.login(loginDetail);
         return result;
+    }
+
+    @GetMapping("/getEnvData")
+    public String getEnvData() {
+        return this.message;
     }
 }
